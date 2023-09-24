887856976832618dd0ec8524b632a11bf400fe81
kimseongj <ksj8277@naver.com>
2023-09-25 00:13:51 +0900

    feat: BoxOfficePresenter 구현

---
+++ BoxOffice/Presenter/BoxOfficeViewPresenter.swift
@@ -0,0 +1,71 @@
+//
+//  BoxOfficeViewPresenter.swift
+//  BoxOffice
+//
+//  Created by kimseongjun on 2023/09/22.
+//
+
+import Foundation
+
+protocol BoxOfficeViewMakable: AnyObject {
+    func renewNavigationBarTitle()
+    func reload()
+}
+
+class BoxOfficePresenter: SendChoosenDateDelegate {
+    weak var boxOfficeView: BoxOfficeViewMakable?
+    var choosenDate: String = "" {
+        didSet {
+            fetchBoxOfficeData {
+                DispatchQueue.main.async {
+                    self.boxOfficeView?.reload()
+                    self.boxOfficeView?.renewNavigationBarTitle()
+                }
+            }
+        }
+    }
+    var dailyBoxOfficeService: DailyBoxOfficeService
+    var dailyBoxOffice: DailyBoxOffice?
+    
+    
+    func receive(choosenDate: String) {
+        self.choosenDate = choosenDate
+    }
+    
+    init(boxOfficeView: BoxOfficeViewMakable, dailyBoxOfficeService: DailyBoxOfficeService) {
+        self.boxOfficeView = boxOfficeView
+        self.dailyBoxOfficeService = dailyBoxOfficeService
+        
+        configureCalendarPresenterDelegate()
+    }
+    
+    func configureCalendarPresenterDelegate() {
+    
+    }
+    
+    func fetchBoxOfficeData(completion: @escaping () -> Void) {
+        dailyBoxOfficeService.fetchDailyBoxOfficeAPI(date: choosenDate) { [weak self] in
+            guard let self = self else { return }
+            
+            self.dailyBoxOffice = $0
+            completion()
+        }
+    }
+    
+    func sendDailyBoxOffice() -> DailyBoxOffice? {
+        
+        return dailyBoxOffice
+    }
+    
+    func getYesterdayDescription() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMdd"
+        let yesterDate = formatter.string(from: Date(timeIntervalSinceNow: -86400))
+        choosenDate = yesterDate
+    }
+}
+
+
+protocol SendChoosenDateDelegate: AnyObject {
+    func receive(choosenDate: String)
+}
