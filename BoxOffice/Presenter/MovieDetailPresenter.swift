9a692207a767df5b81a422c43b3cf1f312c19cee
kimseongj <ksj8277@naver.com>
2023-09-25 23:48:18 +0900

    refactor:  reformString메서드를 ViewController에서 Presenter로 가져옴

--- BoxOffice/Presenter/MovieDetailPresenter.swift
+++ BoxOffice/Presenter/MovieDetailPresenter.swift
@@ -18,7 +18,7 @@ class MovieDetailPresenter {
     var imageSearchService: ImageSearchService
     var thumbnailImageData: Data?
     var movieCode: String
-    var movieName: String = "" 
+    var movieName: String?
     
     init(movieDetailService: MovieDetailService = MovieDetailModel(),
          imageSearchService: ImageSearchService = ImageSearchModel(), movieCode: String) {
@@ -39,7 +39,27 @@ class MovieDetailPresenter {
     func fetchSearchedImage(movieName: String) {
         imageSearchService.fetchSearchedImage(movieName: movieName) { [weak self] in
             guard let self = self else { return }
+            
             self.movieDetailView?.fillImage(iamgeData: $0)
+        }
+    }
+    
+    func reformString(movieDetail: MovieDetail?, labelText: String) -> String {
+        guard let valildMovieDetail = movieDetail else { return "" }
+        
+        switch labelText {
+        case "감독":
+            return valildMovieDetail.movieInformationResult.movieInformation.directors.map{$0.peopleName}.joined(separator: ", ")
+        case "관람등급":
+            return valildMovieDetail.movieInformationResult.movieInformation.audits.map{$0.watchGradeName}.joined(separator: ", ")
+        case "제작국가":
+            return valildMovieDetail.movieInformationResult.movieInformation.nations.map{$0.nationName}.joined(separator: ", ")
+        case "장르":
+            return valildMovieDetail.movieInformationResult.movieInformation.genres.map{$0.genreName}.joined(separator: ", ")
+        case "배우":
+            return valildMovieDetail.movieInformationResult.movieInformation.actors.map{$0.peopleName}.joined(separator: ", ")
+        default:
+            return ""
         }
     }
 }
